e0653f8ed3dfd514cab144127d6ad9de99dca058
Anton Subbotin <kotosharules@gmail.com>
2024-01-13 11:31:59 +0100

    🐌 Static text solution

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
+import { IntlProvider } from 'react-intl'
 import App from './App'
+import messages from '../static/messages.json'
 
 export function render() {
     const html = ReactDOMServer.renderToString(
         <React.StrictMode>
-            <App />
+            <IntlProvider locale="en-us" messages={messages}>
+                <App />
+            </IntlProvider>
         </React.StrictMode>,
     )
     return { html }
